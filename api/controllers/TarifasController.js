85cd0223ff31be3f42d9a3ec112e0dbf17694bd5
CristianG540 <cristianneatproy@gmail.com>
2016-03-31 07:41:59 -0500

    Se crea el modelo de tarifas

---
+++ api/controllers/TarifasController.js
@@ -0,0 +1,11 @@
+/**
+ * TarifasController
+ *
+ * @description :: Server-side logic for managing tarifas
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
+
